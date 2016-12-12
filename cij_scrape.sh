482224c2c1bef896ef79ca22c3fe71c93fcd02ee
fedecarles <fedecarles@gmail.com>
2016-12-12 19:06:24 -0300

    added scraper bash

---
+++ cij_scrape.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Go to directory
+cd /home/fedecarles/legalminer
+
+# Activate virtual env.
+source /home/fedecarles/.virtualenvs/venv/bin/activate
+
+# Run scraper
+python3 /home/fedecarles/legalminer/cij_scrape.py
+
+# Update index
+python3 manage.py update_index --age=5
+
+
